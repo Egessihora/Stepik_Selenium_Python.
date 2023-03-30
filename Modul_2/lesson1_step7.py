c37e69b05acfb6703704f757c0b864c71bdc9c4b
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2023-03-30 19:57:53 +0300

    Add file

---
+++ Modul_2/lesson1_step7.py
@@ -0,0 +1,52 @@
+# MODUL 2. LESSON 1. TASK 7
+
+# Ваша программа должна:
+#
+# Открыть страницу http://suninjuly.github.io/get_attribute.html.
+# Найти на ней элемент-картинку, который является изображением сундука с сокровищами.
+# Взять у этого элемента значение атрибута valuex, которое является значением x для задачи.
+# Посчитать математическую функцию от x (сама функция остаётся неизменной).
+# Ввести ответ в текстовое поле.
+# Отметить checkbox "I'm the robot".
+# Выбрать radiobutton "Robots rule!".
+# Нажать на кнопку "Submit".
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "treasure")  # находим элемент
+    x = x_element.get_attribute("valuex")   # находим значение атрибута у элемента
+    y = calc(x)   # подставляем значение в формулу
+    y_element = browser.find_element(By.ID, "answer")
+    y_element.send_keys(y)
+
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
