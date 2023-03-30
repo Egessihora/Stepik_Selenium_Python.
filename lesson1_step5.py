8faa58f4e751002f894926e1287e1d3992cb9a63
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2023-03-30 19:55:17 +0300

    Add file

---
+++ lesson1_step5.py
@@ -0,0 +1,51 @@
+# MODUL 2. LESSON 1. TASK 5
+
+# Ваша программа должна выполнить следующие шаги:
+#
+# Открыть страницу https://suninjuly.github.io/math.html.
+# Считать значение для переменной x.
+# Посчитать математическую функцию от x.
+# Ввести ответ в текстовое поле.
+# Отметить checkbox "I'm the robot".
+# Выбрать radiobutton "Robots rule!".
+# Нажать на кнопку Submit.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    y_element = browser.find_element(By.ID, "answer")
+    y_element.send_keys(y)
+
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
